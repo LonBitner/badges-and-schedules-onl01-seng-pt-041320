7c7be7f7bed2647da941a38b19d455875f7b06ce
Lon Bitner <lonbitner@gmail.com>
2020-04-18 01:09:03 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,13 +1,13 @@
 people = ["Edsger", "Ada", "Charles", "Alan", "Grace", "Linus", "Matz"]
 
-def batch_badge_maker(name)
-  return "Hello, my name is #{name}."
+def batch_badge_maker(attendees)
+  return "Hello, my name is #{attendees}."
 end
 
 def batch_badge_creator(array)
   new_array = []
-  array.each do |name|
-    new_array << ("Hello, my name is #{name}.")
+  array.each do |attendees|
+    new_array << ("Hello, my name is #{attendees}.")
   end
   return new_array
 end
@@ -15,8 +15,8 @@ end
 def assign_rooms(array)
   new_array = []
   counter = 1
-  array.each do |name|
-    new_array << ("Hello, #{name}! You'll be assigned to room #{counter}")
+  array.each do |attendees|
+    new_array << ("Hello, #{attendees}! You'll be assigned to room #{counter}")
     counter += 1
   end
   return new_array
